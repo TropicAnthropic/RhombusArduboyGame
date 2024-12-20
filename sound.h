09445c5d45a4cc34bade3ce64a87e149594f3ef1
Matthew Bryan <33104756+TropicAnthropic@users.noreply.github.com>
2024-12-20 08:06:45 -0500

    Add files via upload

---
+++ sound.h
@@ -0,0 +1,39 @@
+const uint16_t newaboyFx[] PROGMEM = {
+  800,200, 900,200, 1000,200,
+  TONES_END
+};
+
+const uint16_t onebitFx[] PROGMEM = {
+  1100,25, 1200,25, 1300,100,
+  TONES_END
+};
+
+const uint16_t stompFx[] PROGMEM = {
+  500,50, 600,50,
+  TONES_END,
+};
+
+const uint16_t fixFx[] PROGMEM = {
+  200, 50,  300,50, 400,50,
+  TONES_END
+};
+
+const uint16_t cureFx[] PROGMEM = {
+  500, 50,  600,50, 700,50,
+  TONES_END
+};
+
+const uint16_t rainFx[] PROGMEM = {
+  500, 10,  600,10, 700,10, 800,10, 900,10, 800,10, 700,10, 1000,10, 1000,10,
+  TONES_END
+};
+
+const uint16_t playerDieFx[] PROGMEM = {
+  500,50, 400,50, 300,100, 500,25, 600, 25, 700, 25, 800, 25,
+  TONES_END
+};
+
+const uint16_t changeFx[] PROGMEM = {
+  700, 50,  800,50, 900,50,
+  TONES_END
+};
